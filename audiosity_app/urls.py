3b51726d45cc9ebd7bf5fbbf37bb6a42e6175dee
adse18 <setzadrian@gmail.com>
2024-06-14 15:32:00 +0200

    created an archive

--- audiosity_app/urls.py
+++ audiosity_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('lyrics_analysis/', views.lyrics_analysis, name='lyrics_analysis'),
     path('lyrics2image/', views.lyrics2image, name='lyrics2image'),
     path('image2lyrics/', views.image2lyrics, name='image2lyrics'),
+    path('archive/', views.archive, name='archive'),
+
 ]
--- audiosity_app/views.py
+++ audiosity_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Song
+from django.core.paginator import Paginator
 
 def index(request):
     return HttpResponse("Hello, world. You're at the audiosity_app index.")
@@ -15,3 +17,16 @@ def lyrics2image(request):
 
 def image2lyrics(request):
     return render(request, 'image2lyrics.html')
+
+def archive(request):
+    query = request.GET.get('q')
+    if query:
+        songs = Song.objects.filter(title__icontains=query)
+    else:
+        songs = Song.objects.all()
+
+    paginator = Paginator(songs, 10)  # Show 10 songs per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'archive.html', {'page_obj': page_obj, 'query': query})
