d14f5f9f0c01af84dff27d899c0f4205dd7b798e
adse18 <setzadrian@gmail.com>
2024-06-17 00:38:02 +0200

    implemented vector search against sample songs

--- audiosity_app/views.py
+++ audiosity_app/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from .models import Song
 from django.core.paginator import Paginator
 
+from langchain_huggingface import HuggingFaceEmbeddings
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
 def index(request):
     return HttpResponse("Hello, world. You're at the audiosity_app index.")
 
@@ -32,3 +36,35 @@ def archive(request):
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'archive.html', {'page_obj': page_obj, 'query': query})
+
+def lyrics_analysis(request):
+
+    query = request.GET.get('query', '')
+
+    print(query)
+
+    embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+    db = FAISS.load_local("C:/Users/Adrian/Documents/DHBW/6.Semester/advanced-ml/audiosity_root/audiosity_app/vector_db_lyrics", embedding, allow_dangerous_deserialization=True)
+
+    docs = db.similarity_search(query,k=5)
+
+    print(docs[0])
+
+    result_docs = []
+
+    for x in docs:
+        temp = dict({'source':x.metadata['source'],
+                     'content':x.page_content})
+        result_docs.append(temp)
+
+    #categs = Category.objects.all()[1:5]
+
+    print(result_docs[0])
+
+    ctx = {
+    #    'categories':categs,
+        'results':result_docs
+    }
+
+    return render(request, 'lyrics_analysis.html', ctx)
