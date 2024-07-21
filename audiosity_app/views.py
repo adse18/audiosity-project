dfe5be8c75033f50363ba064a14b5767ac391cd4
adse18 <setzadrian@gmail.com>
2024-07-22 00:12:13 +0200

    merged song dbs and integrated master db in website

--- audiosity_app/views.py
+++ audiosity_app/views.py
@@ -9,6 +9,7 @@ from langchain_community.vectorstores import FAISS
 
 import os
 from django.conf import settings
+from django.db.models import Q
 
 root_path = settings.BASE_DIR
 
@@ -29,12 +30,17 @@ def image2lyrics(request):
 
 def archive(request):
     query = request.GET.get('q', '')
+    
+    # Build the query filters
     if query:
-        songs = Song.objects.filter(song_title__icontains=query)
-        songs = Song.objects.filter(artist__icontains=query)
-
+        songs = Song.objects.filter(
+            Q(title__icontains=query) | Q(artist__icontains=query)
+        ).order_by('title')  # Example ordering by title, change as needed
     else:
-        songs = Song.objects.all()
+        songs = Song.objects.all().order_by('title')  # Example ordering by title
+
+    print(f"Number of songs fetched: {songs.count()}")
+    print(f"Raw SQL Query: {str(songs.query)}")
 
     paginator = Paginator(songs, 20)  # Show 20 songs per page.
     page_number = request.GET.get('page')
